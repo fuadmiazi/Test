7b8fda10c8eac6a3578489e18017ee2a3404d4c1
fuadmiazi <fuad.hasan.1997@gmail.com>
2018-11-06 22:15:41 +0600

    First Commit

---
+++ Prob 1.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+int main()
+{
+    int days,years,weeks,RemainingDays;
+    printf("Number of days: ");
+    scanf("%d",&days);
+    int tempoYear=0, tempoWeeks=0, tempoRemainingDays= 0;
+
+    if((days/365)!=0)
+    {
+        tempoYear = days/365;
+        days = days-(tempoYear*365);
+    }
+        printf("Years: %d",tempoYear);
+
+    if((days/7)!=0)
+    {
+        tempoWeeks = days/7;
+        days = days - (tempoWeeks*7);
+    }
+        printf("\nWeeks: %d",tempoWeeks);
+
+    if((days/24)!=0)
+    {
+        tempoRemainingDays = days/24;
+        printf("Days: %d",tempoRemainingDays);
+    }
+    else
+        printf("\nDays: %d",days);
+
+
+
+
+    return 0;
+
+}
